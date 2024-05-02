cb7702083d26d504b4a21118d1f934d8557fd52d
Tony Chen <tkchen@amazon.com>
2024-05-02 16:44:11 -0700

    docs: update package description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 DIST_NAME = "agent-evaluation"
 VERSION = "0.1.0"
-DESCRIPTION = "A LLM-powered framework for testing virtual agents."
+DESCRIPTION = "A generative AI-powered framework for testing virtual agents."
 AUTHOR = "Amazon Web Services"
 EMAIL = "agent-evaluation-oss-core-team@amazon.com"
 URL = "https://github.com/awslabs/agent-evaluation"
