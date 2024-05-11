3992e3ede23be28e46a86a3a3e967eabc3979721
Tony Chen <tkchen@amazon.com>
2024-05-10 17:03:07 -0700

    feat: make q_business_user_id optional

--- src/agenteval/targets/q_business/target.py
+++ src/agenteval/targets/q_business/target.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from agenteval.target_response import TargetResponse
 from agenteval.targets import Boto3Target
 
@@ -6,31 +8,25 @@ _SERVICE_NAME = "qbusiness"
 
 class QBusinessTarget(Boto3Target):
     def __init__(
-        self, q_business_application_id: str, q_business_user_id: str, **kwargs
+        self,
+        q_business_application_id: str,
+        q_business_user_id: Optional[str] = None,
+        **kwargs
     ):
         super().__init__(boto3_service_name=_SERVICE_NAME, **kwargs)
 
-        self._q_business_application_id = q_business_application_id
-        self._q_business_user_id = q_business_user_id
-        self._parent_message_id: str = ""
-        self._conversation_id: str = ""
+        self._chat_sync_args = {"applicationId": q_business_application_id}
+        if q_business_user_id:
+            self._chat_sync_args["userId"] = q_business_user_id
 
     def invoke(self, prompt: str) -> str:
-        args = {
-            "applicationId": self._q_business_application_id,
-            "userId": self._q_business_user_id,
-            "userMessage": prompt,
-        }
+        self._chat_sync_args["userMessage"] = prompt
 
-        if self._conversation_id and self._parent_message_id:
-            args["conversationId"] = self._conversation_id
-            args["parentMessageId"] = self._parent_message_id
+        response = self.boto3_client.chat_sync(**self._chat_sync_args)
 
-        response = self.boto3_client.chat_sync(**args)
+        if "conversationId" not in self._chat_sync_args:
+            self._chat_sync_args["conversationId"] = response["conversationId"]
 
-        self._parent_message_id = response["systemMessageId"]
-
-        if not self._conversation_id:
-            self._conversation_id = response["conversationId"]
+        self._chat_sync_args["parentMessageId"] = response["systemMessageId"]
 
         return TargetResponse(response=response["systemMessage"])
