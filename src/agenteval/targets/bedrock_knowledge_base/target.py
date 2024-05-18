f3c3f237f47c829da5d045c0f07394a74f5f7309
Tony Chen <tkchen@amazon.com>
2024-05-18 10:32:51 -0700

    chore: add missing license headers

--- src/agenteval/targets/bedrock_knowledge_base/target.py
+++ src/agenteval/targets/bedrock_knowledge_base/target.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from agenteval import TargetResponse
 from agenteval.targets import Boto3Target
 
--- src/agenteval/test/__init__.py
+++ src/agenteval/test/__init__.py
@@ -1,3 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from .test import Test
 from .test_suite import TestSuite
 from .test_result import TestResult
